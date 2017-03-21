824ec96b7f8e423b9e7cea7f403da10babac8d22
Chris <chidsta1@hotmail.com>
2017-03-21 14:26:01 +1100

    finished

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -242,15 +242,16 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    the_wedge1 = []
-
-    for i in range(10):
-        row = []
-        for j in range(i - 1):
-            the_wedge1.append(loops_1c(number_of_items=10, symbol=" "))
-        the_wedge1.append(row)
-    print(the_wedge1)
-    return the_wedge1
+    k = 0
+    rows = 5
+    for i in range(1, rows + 1):
+        for space in range(1, (rows - 1) + 1):
+            print(end=" ")
+        while k != (2 * i - 1):
+            print("* ", end="")
+            k = k + 1
+        k = 0
+        print()
 
 
 def lp(some_kind_of_list, exercise_name):
--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,11 @@ def loop_ranger(start, stop=None, step=1):
 
     Do this using any method apart from just using range()
     """
-    pass
+    first_loop = []
+    while start < stop:
+        first_loop.append(start)
+        start += step
+    return first_loop
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +24,10 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    pass
+    second_loop = []
+    for i in range(start, stop, step):
+        second_loop.append(i)
+    return second_loop
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +36,12 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    pass
+    step = 2
+    third_loop = []
+    while start < stop:
+        third_loop.append(start)
+        start += step
+    return third_loop
 
 
 def gene_krupa_range(start, stop, even_step, odd_step):
@@ -38,7 +50,16 @@ def gene_krupa_range(start, stop, even_step, odd_step):
     make a list that instead of having evenly spaced steps
     has odd steps be one size and even steps be another.
     """
-    pass
+    fourth_loop = []
+    loop = 0
+    while start < stop:
+        fourth_loop.append(start)
+        if loop % 2 == 0:
+            start += even_step
+        else:
+            start += odd_step
+        loop += 1
+    return fourth_loop
 
 
 def stubborn_asker(low, high):
@@ -66,7 +87,7 @@ def super_asker(low, high):
     Combine stubborn_asker and not_number_rejector to make a function
     that does it all!
     """
-    pass
+
 
 if __name__ == "__main__":
     # this section does a quick test on your results and prints them nicely.
